49b71f13c94b64e82078c8120fba2f401c85494e
Appveyor <appveyor@uptheinter.net>
2020-12-28 20:28:23 +0000

    Documentation for 701cf415e3b8e251d297f608d9f9cda6576f03ca

--- search/all_e.js
+++ search/all_e.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['resize_49',['Resize',['../classAssetMap_1_1IMemMapper.html#a29dcee69ac385b9eada643785d57c2df',1,'AssetMap::IMemMapper::Resize()'],['../classAssetMap_1_1MemMapper.html#a7656e576265b4d896e42949b9f5c90ab',1,'AssetMap::MemMapper::Resize(size_t size) override'],['../classAssetMap_1_1MemMapper.html#a7656e576265b4d896e42949b9f5c90ab',1,'AssetMap::MemMapper::Resize(size_t size) override']]],
+  ['resize_49',['Resize',['../classAssetMap_1_1IMemMapper.html#a29dcee69ac385b9eada643785d57c2df',1,'AssetMap::IMemMapper::Resize()'],['../classAssetMap_1_1MemMapper.html#a7656e576265b4d896e42949b9f5c90ab',1,'AssetMap::MemMapper::Resize()']]],
   ['retrieve_50',['Retrieve',['../classAssetMap_1_1MemMappedBucketEntry.html#ae5b5786ecc0b784fe71be7ca591873bc',1,'AssetMap::MemMappedBucketEntry::Retrieve()'],['../classAssetMap_1_1MemMappedBucketEntry.html#aef65ae3c5787dee56f93b697760193d4',1,'AssetMap::MemMappedBucketEntry::Retrieve(uint8_t *buf, size_t len)']]]
 ];
--- search/functions_e.js
+++ search/functions_e.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['setcompresslevel_116',['SetCompressLevel',['../classAssetMap_1_1ZSTD.html#a4c1079487411eabce8c52f3ed4acb9aa',1,'AssetMap::ZSTD']]],
   ['setstrategylevel_117',['SetStrategyLevel',['../classAssetMap_1_1ZSTD.html#af7ab491c5db256cec486fdda0058406b',1,'AssetMap::ZSTD']]],
-  ['size_118',['Size',['../classAssetMap_1_1IMemMapper.html#a40e2bfb89a0eb24523f6480c795f2d60',1,'AssetMap::IMemMapper::Size()'],['../classAssetMap_1_1MemMapper.html#aa6c355f7b961229944f8bfb36a2384a8',1,'AssetMap::MemMapper::Size() const noexcept override'],['../classAssetMap_1_1MemMapper.html#aa6c355f7b961229944f8bfb36a2384a8',1,'AssetMap::MemMapper::Size() const noexcept override']]],
+  ['size_118',['Size',['../classAssetMap_1_1IMemMapper.html#a40e2bfb89a0eb24523f6480c795f2d60',1,'AssetMap::IMemMapper::Size()'],['../classAssetMap_1_1MemMapper.html#aa6c355f7b961229944f8bfb36a2384a8',1,'AssetMap::MemMapper::Size()']]],
   ['strategyinfo_119',['StrategyInfo',['../classAssetMap_1_1ZSTD.html#a6761ad7e1783b03808568cf48114bce2',1,'AssetMap::ZSTD']]]
 ];
